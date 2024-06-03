80853d885baf58cc770eef9298c000f8f7c7a081
kartikeya700 <kartikeyamittal@outlook.com>
2024-06-04 01:04:11 +0530

    Update twttr.py

--- WEEK 2/twttr.py
+++ WEEK 2/twttr.py
@@ -1,6 +1,7 @@
 def main():
     text = input("Input: ")
     vowel = ["a", "e", "i", "o", "u"]
+    print("Output: ", end="")
     for c in text:
         if c.lower() in vowel:
             print("", end="")
